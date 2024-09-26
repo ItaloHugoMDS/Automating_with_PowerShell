b5269109b6a7ae05106808e3ab5e837c402c5b5f
Ítalo Hugo <109737253+ItaloHugoMDS@users.noreply.github.com>
2024-09-26 11:15:08 -0300

    New Files

--- Call.ps1
+++ Call.ps1
@@ -2,17 +2,20 @@
 
 function Create-YearPath
 {
-    New-Item -ItemType Directory -Name $Year -Path $Year_Path
+    Write-Host "Entered the Create-YearPath function"
+    New-Item -ItemType Directory -Name $Year -Path $Root_Path
 }
 
 function Create-MonthPath
 {
-    New-Item -ItemType Directory -Name $Month -Path $Month_Path
+    Write-Host "Entered the Create-MonthPath function"
+    New-Item -ItemType Directory -Name $Month -Path $Year_Path
 }
 
 function Create-DayPath
 {
-    New-Item -ItemType Directory -Name $Day -Path $Day_Path
+    Write-Host "Entered the Create-DayPath function"
+    New-Item -ItemType Directory -Name $Day -Path $Month_Path
 }
 
 if (!(Test-Path -Path ".\Calls"))    # Checking is the "Setup" script was executed already.
@@ -22,36 +25,40 @@ if (!(Test-Path -Path ".\Calls"))    # Checking is the "Setup" script was execut
     Exit
 }
 
-$File_Timestamp = $(Get-Date -Format "yyyy-MM-dd---HH-mm-ss") + ".txt"
-$Year = Get-Date -Format "yyyy"
-$Month = Get-Date -Format "MM"
-$Day = Get-Date -Format "dd"
-
-$Year_Path = $pwd.path + "\Calls"
-
-if (!(Test-Path -Path $Year_Path))
+function Main
 {
-    Create-YearPath
+    $File_Timestamp = $(Get-Date -Format "yyyy-MM-dd---HH-mm-ss") + ".txt"
+    $Year = Get-Date -Format "yyyy"
+    $Month = Get-Date -Format "MM"
+    $Day = Get-Date -Format "dd"
+    
+    $Root_Path = "{0}\{1}" -f $pwd.path, "Calls"
+    $Year_Path = "{0}\{1}\{2}" -f $pwd.path, "Calls", $Year
+    $Month_Path = "{0}\{1}\{2}\{3}" -f $pwd.path, "Calls", $Year, $Month
+    $Day_Path = "{0}\{1}\{2}\{3}\{4}" -f $pwd.path, "Calls", $Year, $Month, $Day
+    $Current_Path = "{0}\{1}\{2}\{3}\{4}\" -f $pwd.path, "Calls", $Year, $Month, $Day
+    
+    if (!(Test-Path -Path $Year_Path))
+    {
+        Create-YearPath
+    }
+    
+    if (!(Test-Path -Path $Month_Path))
+    {
+        Create-MonthPath
+    }
+    
+    if (!(Test-Path -Path $Day_Path))
+    {
+        Create-DayPath
+    }
+    
+    $Message_Template = "First Name: `nLast Name: `nID: `nLocation: `nCallBack Number: `nProblem Description: "
+    
+    New-Item -ItemType File -Name $File_Timestamp -Path $Current_Path -Value $Message_Template
+    
+    Invoke-Item $($Current_Path + $File_Timestamp)
+
 }
 
-$Month_Path = $Year_Path + "\$Year"
-
-if (!(Test-Path -Path $Month_Path))
-{
-    Create-MonthPath
-}
-
-$Day_Path = $Month_Path + "\$Month"
-
-if (!(Test-Path -Path $Day_Path))
-{
-    Create-DayPath
-}
-
-$Current_Path = $Day_Path + "\$Day\"
-
-$Message_Template = "First Name: `nLast Name: `nID: `nLocation: `nCallBack Number: `nProblem Description: "
-
-New-Item -ItemType File -Name $File_Timestamp -Path $Current_Path -Value $Message_Template
-
-Invoke-Item $($Current_Path + $File_Timestamp)
+Main
